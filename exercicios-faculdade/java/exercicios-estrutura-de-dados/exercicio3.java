a47989b86f67906cf13171a6741c71e400686a80
Raul Anselmo D Santos <raul.trabalho5511@gmail.com>
2023-02-26 13:53:40 -0300

    ok

--- exercicios-faculdade/java/exercicios-estrutura-de-dados/exercicio3.java
+++ exercicios-faculdade/java/exercicios-estrutura-de-dados/exercicio3.java
@@ -1,9 +1,4 @@
 public class exercicio3 {
-        
-    public static void todos_crescente(int n){
-        if(n>=0){
-            todos_crescente(n-1);
-            System.out.println(n);
-        }
-    }
+    
+    
 }
